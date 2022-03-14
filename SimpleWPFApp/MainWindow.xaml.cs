27f5616ce42f177b9ee07667ecff5f6d664f9c09
Rio Surya Fachrudin <32932036+riosurya12@users.noreply.github.com>
2022-03-14 21:55:05 +0700

    Add files via upload

---
+++ SimpleWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            resultTextBlock.Text = "...";
+        }
+
+        private void HelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!resultTextBlock.Text.Equals("Hello, World"))
+            {
+                resultTextBlock.Text = "Hello, world";
+            }
+            else
+            {
+                resultTextBlock.Text = "Hai, this is WPF";
+            }
+
+            MessageBox.Show("hello, world. Ini WPF saya.", "Ini program WPF pertama", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+
+            string caption = "Halo gesss";
+            MessageBoxButton btnOK = MessageBoxButton.OK;
+            MessageBoxImage imgInfo = MessageBoxImage.Information;
+
+            string message = $"hello, world {Environment.NewLine}Hello gess";
+            MessageBox.Show(message, caption, btnOK, imgInfo);
+        }
+    }
+}
